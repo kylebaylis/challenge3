afae1bad7fd4c12be9ef59913ee2a202369f228b
Kyle <kylejmbaylis@gmail.com>
2022-03-11 17:29:01 -0500

    fixed number input, can only input number now

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,14 +8,14 @@ passChar = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
+  // asking if you want to make a password, if not then quit
   var makePass = window.confirm("Would you like to generate a password? Click OK to continue.");
-
+  // to determine what is in password
   if (makePass) {
     var passLength = window.prompt("How long would you like your password to be? Pick a number from 8 to 128.");
-    var passLengthNum = parseInt(passLength);
-    while (passLength < 8 || passLength > 128) {
+    // won't proceed unless value is a number between 8 and 128
+    while (isNaN(passLength) || passLength < 8 || passLength > 128) {
       var passLength = window.prompt("Please pick a number from 8 to 128.");
-      var passLengthNum = parseInt(passLength);
     }
     if (passLength) {
       var lowerCase = window.confirm("Would you like at least one lowercase character? OK for yes, Cancel for no.");
@@ -38,7 +38,7 @@ function writePassword() {
         passContent = passContent + passChar;
       }
     }
-    window.alert(passContent);
+    // window.alert(passContent); to see if each selection works
   }
   else {
     window.alert("Click 'Generate Password' if you change your mind!");
